6643d15de22a3ef48010a7f0c57ae4048ce776da
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-10-19 09:18:30 +0530

    [Prathamesh Rajput] Add. Completed Use Case 1

--- Person.cs
+++ Person.cs
@@ -8,5 +8,18 @@ namespace PersonDataManagement
 {
     class Person
     {
+        public string SSN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(string SSN, string Name, string Address, int Age)
+        {
+            this.SSN = SSN;
+            this.Name = Name;
+            this.Address = Address;
+            this.Age = Age;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,16 @@ namespace PersonDataManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Person Data Management Problem");
+            List<Person> People = new List<Person>();
+            AddDefaultData(People);
+        }
+
+        public static void AddDefaultData(List<Person> people)
+        {
+            people.Add(new Person("SSN1", "Name 1", "Address 1", 21));
+            people.Add(new Person("SSN2", "Name 2", "Address 2", 21));
+            people.Add(new Person("SSN3", "Name 3", "Address 3", 21));
+            people.Add(new Person("SSN4", "Name 4", "Address 4", 21));
         }
     }
 }
